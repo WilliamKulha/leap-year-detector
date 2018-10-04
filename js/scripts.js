52ed4db8e7a94fc7111954f27eb660273e554342
William Kulha <kulha.william@gmail.com>
2018-10-04 11:23:36 -0400

    Add logic so that if the year is divisible by 100 the function returns false.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,10 @@
 let leapYear = function(year) {
-  if (year % 4 === 0) {
-    return true
+  if (year % 100 === 0) {
+    return false;
+  } else if (year % 4 === 0) {
+    return true;
   } else {
-    return false
+    return false;
   }
 }
 
