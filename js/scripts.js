ca01951ec5a2894aece38e129575439c47bc9300
William Kulha <kulha.william@gmail.com>
2018-10-04 11:38:19 -0400

    Add user interface logic so that the result will display in a sentence rather than true or false.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+//business logic
 let leapYear = function(year) {
   if (year % 4 === 0 && year % 100 !== 0 || year % 400 === 0) {
     return true;
@@ -5,14 +6,21 @@ let leapYear = function(year) {
     return false;
   }
 }
-
-
-
+//user logic
 $(document).ready(function() {
   $('#leap-year').submit(function(event) {
     event.preventDefault();
     var year = parseInt($('input#year').val());
     var result = leapYear(year);
-    $('#result').text(result);
-  })
-})
+
+    $('.year').text(year);
+
+    if (!result) {
+      $('.not').text('not');
+    } else {
+      $('.not').text("");
+    }
+
+    $('#result').show();
+  });
+});
